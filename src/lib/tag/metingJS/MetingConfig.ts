95d07a7836fd2544b7b949b168e7dd6c44d96528
MonoLogueChi <xxwhite.com@gmail.com>
2021-02-24 13:52:53 +0800

    修复 meting api 设置失效问题

--- src/lib/tag/metingJS/MetingConfig.ts
+++ src/lib/tag/metingJS/MetingConfig.ts
@@ -2,6 +2,6 @@ class MetingConfig extends BaseConfig {
   aplayer_css: string = config_default.aplayer.css;
   aplayer_js: string = config_default.aplayer.js;
   meting_js: string = config_default.meting.js;
-  meting_api: string = "";
+  meting_api: string = config_default.meting.api;
   data: { [key: string]: string | number | boolean | JSON } = config_default.meting.default || {};
 }
